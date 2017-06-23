496bd21119ff847c3e9be340f1e692d5522e25f5
Brian Cooley-Gilliom <brian@calvium.com>
2017-06-23 13:20:53 +0100

    removing ImageFit, as it causes warnings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Console from './Console';
 import ExtendableError from './ExtendableError';
 import Utilities from './Utilities';
-import ImageFit from './ImageFit';
 
-export {Console, ExtendableError, Utilities, ImageFit};
-
+export {Console, ExtendableError, Utilities};
